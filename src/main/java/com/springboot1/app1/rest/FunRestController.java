d2751739c8af2c95c59f6206d460fbd92afddccd
Omar Hosny <102857390+Omar-Hosny1@users.noreply.github.com>
2024-01-13 16:53:49 +0200

    finished

--- src/main/java/com/springboot1/app1/rest/FunRestController.java
+++ src/main/java/com/springboot1/app1/rest/FunRestController.java
@@ -1,11 +1,14 @@
 package com.springboot1.app1.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // To Set up a rest controller
-public class FunRestController {
-
+class FunRestController {
+    @Value("${my.name}")
+    private String myName;
 
     // expose "/" EP that returns "Hello World"
     @GetMapping("/") // To Handle HTTP get Requestcs
@@ -16,5 +19,14 @@ public class FunRestController {
     @GetMapping("/workout")
     public String getDailyWorkout() {
         return "Go To Gym";
+    }
+    @GetMapping("/my-name")
+    public String getMyName() {
+        return myName;
+    }
+
+    @PostMapping("/add-book")
+    public String postBook() {
+        return "Added";
     }
 }
